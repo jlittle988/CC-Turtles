6fcefede594c10d026135d8a676d6af8e45241a0
John Little <johnljr988@gmail.com>
2020-12-22 16:40:59 -0500

    Update Quarry.lua

--- Quarry.lua
+++ Quarry.lua
@@ -1,9 +1,9 @@
 -- length, width, depth (levels)
 args = {...}
 
-int length = args[1]
-int width = args[2]
-int depth = args[3]
+length = args[1]
+width = args[2]
+depth = args[3]
 
 -- dig up down and front, and check to make sure no new block fell into place (gravel)
 function dig()
