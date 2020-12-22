a45badbec44f87d2b3f8afaee2cc23959509fbbf
John Little <johnljr988@gmail.com>
2020-12-22 17:07:55 -0500

    Update Quarry.lua

--- Quarry.lua
+++ Quarry.lua
@@ -1,5 +1,5 @@
 FUEL_ITEM = "minecraft:coal"
-REFUEL_AT = 5 -- Fuel level at which the turtle will refuel
+REFUEL_AT = 22 -- Fuel level at which the turtle will refuel
 REFUEL_COUNT = 1 -- How many fuel items to consume at each refuel
 
 -- length, width, depth (levels)
