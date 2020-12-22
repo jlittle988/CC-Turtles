20c2e4570550ab999d4a2800b69b260b2c496599
John Little <johnljr988@gmail.com>
2020-12-22 16:39:31 -0500

    Update Quarry.lua

--- Quarry.lua
+++ Quarry.lua
@@ -1,1 +1,14 @@
-turtle.dig()
+-- length, width, depth (levels)
+args = {...}
+
+int length = args[1]
+int width = args[2]
+int depth = args[3]
+
+-- dig up down and front, and check to make sure no new block fell into place (gravel)
+function dig()
+    while turtle.detect()==1 do
+        turtle.dig()
+    end
+
+dig()
