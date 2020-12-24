56d8a590f797ea7b5bc0b87da7df9e1f35d67380
John Little <johnljr988@gmail.com>
2020-12-23 21:09:40 -0500

    Create Lumberjack.lua

---
+++ Lumberjack.lua
@@ -0,0 +1,97 @@
+LOG_BLOCK = 'minecraft:oak_log'
+FUEL_ITEM = 'minecraft:coal'
+
+-- heading relative to placement orientation
+heading = 0
+-- 0: forward
+-- 1: right
+-- 2: backward
+-- 3: left
+
+function turnRight()
+    turtle.turnRight()
+    heading=heading+1
+    if heading>3 then heading=0 end
+end
+
+function turnLeft()
+    turtle.turnLeft()
+    heading=heading-1
+    if heading<0 then heading=3 end
+end
+
+function faceHeading(newHeading)
+    while not (newHeading-heading==0) do
+        if (newHeading-heading)>0 then turnRight()
+        elseif (newHeading-heading)<0 then turnLeft() end
+    end
+end
+
+-- Returns true if item is important and should stay in inventory
+function isItemImportant(slot)
+    if turtle.getItemCount(slot)==0 then return false end
+    item = turtle.getItemDetail(slot)['name']
+    return item==FUEL_ITEM
+end
+
+-- Returns the slot number of item
+function findItem(item)
+    for slot=1,16 do
+        if turtle.getItemCount(slot)>0 then
+            if turtle.getItemDetail(slot)['name'] == item then
+                return slot
+            end
+        end
+    end
+    return nil
+end
+
+-- Checks fuel level and refuels if needed
+function checkFuel()
+    if turtle.getFuelLevel() <= REFUEL_AT then
+        slot = findItem(FUEL_ITEM)
+        if not slot then error('Out of fuel') end
+        print('Refueling')
+        turtle.select(slot)
+        turtle.refuel(REFUEL_COUNT)
+    end
+end
+
+function chopTree()
+    faceHeading(0) -- Face forward
+    
+    -- Dig the tree
+    i=0
+    while turtle.inspect()['name']==LOG_BLOCK do
+        turtle.dig()
+        turtle.digUp()
+        turtle.up()
+        i=i+1
+    end
+
+    -- Go back down
+    while i>0 do
+        turtle.down()
+        i=i-1
+    end
+end
+
+function deposit()
+    faceHeading(2) -- Face backward
+
+    -- Drop all unimportant items
+    for slot=1,16 do
+        if not isItemImportant(slot) then
+            turtle.select(slot)
+            turtle.drop()
+        end
+    end
+end
+
+while true do
+    if turtle.inspect()['name']==LOG_BLOCK then
+        checkFuel()
+        chopTree()
+        deposit()
+    end
+end
