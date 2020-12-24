d3892ed08d425d2321e28ad96fa13161adc58c98
John Little <johnljr988@gmail.com>
2020-12-23 21:10:30 -0500

    Updated startup scripts

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 shell.run('rm', 'Quarry.lua')
 shell.run('wget', 'https://raw.githubusercontent.com/jlittle988/CC-Turtles/main/Quarry.lua', 'Quarry.lua')
+
+shell.run('rm', 'Lumberjack.lua')
+shell.run('wget', 'https://raw.githubusercontent.com/jlittle988/CC-Turtles/main/Lumberjack.lua', 'Lumberjack.lua')
